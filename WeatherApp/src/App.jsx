b623cb7ca1b9c034df12b7c91fbe55bb4d690683
zakzak45 <147825634+zakzak45@users.noreply.github.com>
2025-04-26 22:06:10 +0200

    initialcommit

---
+++ WeatherApp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Weather from './WeatherApp.jsx'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Weather/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ WeatherApp/src/WeatherApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+
+const Weather = () => {
+    return ( 
+        <h1>hello khunju</h1>
+     );
+}
+ 
+export default Weather;
