47b9ccc54bcddf928027c81eaeb07643d1f1d831
zakzak45 <147825634+zakzak45@users.noreply.github.com>
2025-05-13 15:35:20 +0200

    updated  the weather

--- weatherApp/src/main.js
+++ weatherApp/src/main.js
@@ -1,15 +1,21 @@
-let city;
-const API_URL ="https://api.openweathermap.org/data/2.5/weather?q=`${city}`&appid=9545317b796bbc74686fe9e1b0dce392&units=metric"
+const apikey ="9545317b796bbc74686fe9e1b0dce392"
+const apiURL ="https://api.openweathermap.org/data/2.5/weather?&units=metric&q="
 
+const search  =document.querySelector('.search input')
+const searchBtn  =document.querySelector('.search button')
 
 async function CheckWeather(city){
-  const response=await fetch(API_URL)
+  const response=await fetch(apiURL +city+`&appid=${apikey}`)
   var data = await response.json()
   console.log(data)
 
   document.querySelector('.city').innerHTML=data.name;
+  document.querySelector('.temp').innerHTML=data.main.temp;
+  document.querySelector('.humidity').innerHTML=data.main.humidity;
 }
 
+searchBtn.addEventListener('click',()=>{
+  CheckWeather(search.value)
+})
 
-CheckWeather();
 
